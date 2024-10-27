bbe48f5bb404ee5e9cc75bedbbe143e0a3f10115
pho86 <philip03ho@gmail.com>
2024-10-27 16:21:03 -0700

    fix h1 font weight, and html title

--- src/components/Headline/index.tsx
+++ src/components/Headline/index.tsx
@@ -11,7 +11,7 @@ export default function Headline({
     <header className="flex flex-col gap-3 text-left text-pretty sm:text-center px-6 lg:px-8 sm:w-10/12 md:w-11/12 lg:w-8/12">
       <section className="flex flex-col gap-2">
         <label className="text-primary text-sm font-semibold">{label}</label>
-        <h1 className="font-bold text-3xl text-balance sm:text-pretty ">{headline}</h1>
+        <h1 className="font-semibold text-3xl text-balance sm:text-pretty ">{headline}</h1>
       </section>
       <p className="text-pretty">{description}</p>
     </header>
